7031415c2927efd233c0b3eb86177cbf32239b85
ossama assaghir <59144753+ossamasgr@users.noreply.github.com>
2022-08-16 13:02:09 +0100

    Update znssCustomScriptExtension-flag.sh

--- scripts/znssCustomScriptExtension-flag.sh
+++ scripts/znssCustomScriptExtension-flag.sh
@@ -20,6 +20,7 @@ echo "Initiating ZSOS configuration"
 echo "Create dependency file"
 sudo touch /sc/conf/sc.conf
 
+
 # Install NSS Certificate
 if ! [ -f "NssCertificate.zip" ]; then
     echo "The file NssCertificate.zip was not found."
@@ -29,8 +30,7 @@ if ! [ -f "NssCertificate.zip" ]; then
 fi
 
 echo "Installing Certificate"
-sudo nss install-cert NssCertificate.zip
-
+echo zsroot | sudo -S nss install-cert NssCertificate.zip
 # Get private ip and subnet mask for Service Interface
 SMNET_IP=$(curl -H Metadata:true --silent "http://169.254.169.254/metadata/instance/network/interface/1/ipv4/ipAddress/0/privateIpAddress?api-version=2021-12-13&format=text")
 SMNET_MASK=$(curl -H Metadata:true --silent "http://169.254.169.254/metadata/instance/network/interface/1/ipv4/subnet/0/prefix?api-version=2021-12-13&format=text")
@@ -38,43 +38,43 @@ SMNET_MASK=$(curl -H Metadata:true --silent "http://169.254.169.254/metadata/ins
 # NSS Service Interface and Default Gateway IP Configuration
 echo "Set IP Service Interface IP Address and Default Gateway"
 smnet_dflt_gw=$smnet_dflt_gw
-sudo nss configure --cliinput ${SMNET_IP}"/"${SMNET_MASK},${smnet_dflt_gw}
-echo "Successfully Applied Changes"
+echo zsroot | sudo -S nss configure --cliinput ${SMNET_IP}"/"${SMNET_MASK},${smnet_dflt_gw}
+
 
 # Updading FreeBSD.conf Packages
 echo "Updading FreeBSD.conf Packages"
-sudo mkdir -p /usr/local/etc/pkg/repos
+echo zsroot | sudo -S mkdir -p /usr/local/etc/pkg/repos
 echo "FreeBSD: { enabled: no }" > /usr/local/etc/pkg/repos/FreeBSD.conf
 echo "FreeBSD: { url: "http://13.66.198.11/FreeBSD:11:amd64/latest/", enabled: yes}" > /usr/local/etc/pkg/repos/FreeBSD.conf
-sudo pkg update && pkg check -d -y
-sudo mkdir /sc/build/24pkg-update
+echo zsroot | sudo -S pkg update && pkg check -d -y
+echo zsroot | sudo -S mkdir /sc/build/24pkg-update
 
 # Download NSS Binaries
-sudo nss update-now
+echo zsroot | sudo -S nss update-now
 echo "Connecting to server..."
 echo "Downloading latest version" # Wait until system echo back the next message
 echo "Installing build /sc/smcdsc/nss_upgrade.sh" # Wait until system echo back the next message
 echo "Finished installation!"
 
  #Check NSS Version
-sudo nss checkversion
+echo zsroot | sudo -S nss checkversion
 
 # Start NSS Service
-sudo nss start
+echo zsroot | sudo -S nss start
 echo "NSS service running."
 
 # Enable the NSS to start automatically
-sudo nss enable-autostart
+echo zsroot | sudo -S nss enable-autostart
 echo "Auto-start of NSS enabled "
 
 # Dump all Important Configuration
 mkdir nss_dump_config
-sudo netstat -r > nss_dump_config/nss_netstat.log
-sudo nss dump-config > nss_dump_config/nss_dump_config.log
-sudo nss checkversion > nss_dump_config/nss_checkversion.log
-sudo nss troubleshoot netstat|grep tcp > nss_dump_config/nss_netstat_grep_tcp.log
-sudo nss test-firewall > nss_dump_config/nss_test_firewall.log
-sudo nss troubleshoot netstat > nss_dump_config/nss_troubleshoot_netstat.log
+echo zsroot | sudo -S netstat -r > nss_dump_config/nss_netstat.log
+echo zsroot | sudo -S nss dump-config > nss_dump_config/nss_dump_config.log
+echo zsroot | sudo -S nss checkversion > nss_dump_config/nss_checkversion.log
+echo zsroot | sudo -S nss troubleshoot netstat|grep tcp > nss_dump_config/nss_netstat_grep_tcp.log
+echo zsroot | sudo -S nss test-firewall > nss_dump_config/nss_test_firewall.log
+echo zsroot | sudo -S nss troubleshoot netstat > nss_dump_config/nss_troubleshoot_netstat.log
 /sc/bin/smmgr -ys smnet=ifconfig > nss_dump_config/nss_smnet_ifconfig.log
 cat /sc/conf/sc.conf | egrep "smnet_dev|smnet_dflt_gw" > nss_dump_config/nss_dump_config.log
 
